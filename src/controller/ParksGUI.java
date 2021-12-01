91738533854bc0a3e7a6c7c830c2deecfa70bbcd
JhormanMera <1010159945@u.icesi.edu.co>
2021-11-30 21:11:03 -0500

    Mainpane functional

---
+++ src/controller/ParksGUI.java
@@ -0,0 +1,11 @@
+package controller;
+import model.Maps;
+
+public class ParksGUI {
+	
+	private Maps park;
+	public ParksGUI(Maps p) {
+		park=p;
+	}
+
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,38 @@
 package ui;
 
-public class Main {
+import model.Maps;
+import controller.ParksGUI;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	private Maps map;
+	private ParksGUI pGUI;
+
+	public Main() {
+		map = new Maps();
+		pGUI = new ParksGUI(map);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("screens/mainPane.fxml"));
+		fxmlloader.setController(pGUI);
+		Parent root = fxmlloader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.setResizable(false);
+	}
 
 }
